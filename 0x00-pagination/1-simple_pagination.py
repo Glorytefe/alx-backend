e35ebf7b8afab5c46e04113f27b85fb39a0f9731
Glorytefe <glorytefe@gmail.com>
2024-09-06 22:49:51 +0100

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,6 +38,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """returns data for page"""
+
+        assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset() or []
